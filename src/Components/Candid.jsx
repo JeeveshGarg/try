8778914c7e03cd7d4e3f6c686cc9083cbc0b30de
Sahil <sohil170246@gmail.com>
2021-09-13 08:37:24 +0530

    typeform aded

--- src/Components/Candid.jsx
+++ src/Components/Candid.jsx
@@ -1,36 +1,11 @@
 import React from "react";
-import * as ReactBootStrap from "react-bootstrap";
+
 
 function Candid(props) {
   return (
-    <div id="candid">
-      <ReactBootStrap.Row>
-        <ReactBootStrap.Container
-          style={{ padding: "0" }}
-          className="strip__back"
-        >
-          <h1 className="candidh">
-            Candid Parties
-            <br />& Ambassadors
-          </h1>
-             
-        <ReactBootStrap.Container>
-        
-            <div style={{paddingBottom:"50px"}}>  
-          <ol style={{color:"white",fontSize:"28px",fontWeight:"700"}}>
-            <li>Visit our website</li>
-            <li>Sign up</li>
-            <li>Find the course you want</li>
-          </ol>
-          <button className="candidbtn">Be a Candid Ambassador</button>
-          </div>
-        </ReactBootStrap.Container>
-        </ReactBootStrap.Container>
-      </ReactBootStrap.Row>
-   
-      
-    
-    </div>
+
+ <div data-tf-widget="TzmRgDEb" data-tf-hide-headers data-tf-hide-footer style={{width:"100%",height:"500px", marginTop:"120px"}}></div>
+
   );
 }
 
