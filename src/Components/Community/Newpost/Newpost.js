eefc0c91f6c9b7ff477e0323a8b96f534705e11c
Danish Ahmed Mirza <danishmirza30602@gmail.com>
2021-12-09 17:11:38 +0530

    Fix layout issues on newpost page

--- src/Components/Community/Newpost/Newpost.js
+++ src/Components/Community/Newpost/Newpost.js
@@ -1,24 +1,22 @@
-import React from "react";
-import { Card, Form } from "react-bootstrap";
-import myra from "./myra.svg";
-import sw from "./sw.svg";
-import Post from "./Post.svg";
-import {Dropdown} from "react-bootstrap"
-import mes from "./mes.svg";
+import React from 'react'
+import { Card, Form } from 'react-bootstrap'
+import myra from './myra.svg'
+import sw from './sw.svg'
+import Post from './Post.svg'
+import { Dropdown } from 'react-bootstrap'
+import mes from './mes.svg'
 
-import "./Newpost.css";
+import './Newpost.css'
 
 import Chevron from './Chevron.svg'
 const Newpost = () => {
   return (
     <div
-      className="newpost w-screen md:w-full  w-full flex flex-row md:h-full md:flex md:ml-1 ml-4 md:mr-1 mr-40"
+      className="newpost w-screen md:w-full "
       style={{
-        backgroundColor: "white",
-        marginTop: "18px",
-        borderRadius: "8px",
-      }}
-    >
+        marginTop: '18px',
+        borderRadius: '8px'
+      }}>
       {/* <div className="block md:hidden">
         <Card className="Card">
           <div className="flex">
@@ -37,78 +35,77 @@ const Newpost = () => {
           </Card.Body>
         </Card>
       </div> */}
-      
-      <div>
-        <div className="pl-6 pt-16 md:pr-1 pr-32 pl-12 md:w-80">
-          <h3 className="pb-3"style={{ fontWeight: "bold",paddingBottom:'' }}>New Posts</h3>
-          <div >
-           <textarea className="for pl-2 shadow-inner w-99" style={{width:'700px',borderTop:'solid lightgrey 3px',paddingTop:'4px',borderLeft:'solid lightgrey 1px', borderRight:'solid lightgrey 1px',borderRadius:'5px',height:'350px'}} ></textarea>
-           {/* <div className="h-8 ml-2  flex justify-between" style={{backgroundColor:'#F0F0F0',paddingTop:'1px',marginTop:'10px',border:'solid lightgrey 1px',borderRadius:'3px',width:'680px'}}>
-            <div className="" style={{color:'grey',paddingLeft:'12px',fontSize:'12px'}}>Mention tag (at max 3)</div>
-             <div>
-               <img className="pr-2 pt-2" style={{paddingLeft:'500px'}} src={Chevron}/>
-             </div>
-           </div> */}
 
-<Dropdown>
-  <Dropdown.Toggle style={{backgroundColor:'#F0F0F0',marginTop:'10px',border:'solid lightgrey 1px',borderRadius:'3px',color:'grey',width:'680px'}} id="">
-    <div className="text-xl h-2 pt-1" style={{paddingRight:'550px'}}>
-    Add tags(at max 3)</div>
+      <div className="flex">
+        <div className="py-8 px-8 w-5/6 md:w-11/12  mx-auto bg-white rounded-xl">
+          <h3 className="pb-3" style={{ fontWeight: 'bold', paddingBottom: '' }}>
+            New Posts
+          </h3>
+          <div>
+            <textarea
+              className="w-full h-96 border p-4 rounded-lg"
+              style={{
+                borderTop: 'solid lightgrey 3px',
+                borderLeft: 'solid lightgrey 1px',
+                borderRight: 'solid lightgrey 1px'
+              }}></textarea>
+            {/* <div
+              className="h-8 ml-2  flex justify-between"
+              style={{ backgroundColor: '#F0F0F0', paddingTop: '1px', marginTop: '10px', border: 'solid lightgrey 1px', borderRadius: '3px', width: '680px' }}>
+              <div className="" style={{ color: 'grey', paddingLeft: '12px', fontSize: '12px' }}>
+                Mention tag (at max 3)
+              </div>
+              <div>
+                <img className="pr-2 pt-2" style={{ paddingLeft: '500px' }} src={Chevron} />
+              </div>
+            </div> */}
 
-    <div>
-               <img className="" style={{paddingLeft:'640px'}} src={Chevron}/>
-             </div>
-  </Dropdown.Toggle>
+            <Dropdown>
+              <Dropdown.Toggle
+                className="w-full"
+                style={{ backgroundColor: '#F0F0F0', marginTop: '10px', border: 'solid lightgrey 1px', borderRadius: '3px', color: 'grey' }}
+                id="">
+                <div className="text-xl h-2 pt-1 pr-96 md:pr-52">Add tags(at max 3) </div>
+              </Dropdown.Toggle>
 
-  <Dropdown.Menu>
-    <Dropdown.Item style={{width:'650px'}} href="#/action-1">Confession</Dropdown.Item>
-    <Dropdown.Item href="#/action-2">Date Ideas</Dropdown.Item>
-    <Dropdown.Item href="#/action-3">Rebound</Dropdown.Item>
-    <Dropdown.Item href="#/action-3">LDR</Dropdown.Item>
-  </Dropdown.Menu>
-</Dropdown>
-           
+              <Dropdown.Menu>
+                <Dropdown.Item href="#/action-1">Confession</Dropdown.Item>
+                <Dropdown.Item href="#/action-2">Date Ideas</Dropdown.Item>
+                <Dropdown.Item href="#/action-3">Rebound</Dropdown.Item>
+                <Dropdown.Item href="#/action-3">LDR</Dropdown.Item>
+              </Dropdown.Menu>
+            </Dropdown>
           </div>
 
           <div className="flex justify-between">
-
-           <div className="flex justify-between">
-            {/* <div className="img-col pt-4">
+            <div className="flex justify-between">
+              {/* <div className="img-col pt-4">
              <img src={sw}/>
             </div> */}
-            <div>
-              <label className="switch">
-                <input type="checkbox"/>
-                <span className="slider"/>
-              </label>
-            </div>
-            <div className="text-col pt-4 pl-2 text-lg text-gray-400">
-              Post as anonymous
-            </div>
-            <div className=" abcd img-col mb-32 pt-10 flex" style={{paddingLeft:'420px'}}>
-             <img src={mes}/>
-            </div>
-            <div className="img-col pt-4 pl-6 pr-20  ">
-              {/* <button onClick> <img src={Post}/></button> */}
-              <div className="">
-              <button class="group bg-gray-200 focus:bg-pink-500  ..." style={{width:'70px',height:'40px',borderRadius:'5px'}}><div className="text-white">
-                POST
+              <div>
+                <label className="switch">
+                  <input type="checkbox" />
+                  <span className="slider" />
+                </label>
               </div>
- 
-
-</button>
+              <div className="text-col pt-4 pl-2 text-lg text-gray-400">Post as anonymous</div>
+              {/* <div className=" abcd img-col mb-32 pt-10 flex" style={{ paddingLeft: '420px' }}>
+                <img src={mes} />
+              </div> */}
+              <div className="img-col pt-4 pl-6 pr-20  ">
+                {/* <button onClick> <img src={Post}/></button> */}
+                <div className="">
+                  <button class="group bg-gray-200 focus:bg-pink-500  ..." style={{ width: '70px', height: '40px', borderRadius: '5px' }}>
+                    <div className="text-white">POST</div>
+                  </button>
+                </div>
               </div>
-              
-             
             </div>
-
-           </div>
-
           </div>
         </div>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default Newpost;
+export default Newpost
